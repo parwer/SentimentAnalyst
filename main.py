ab72b1359c53ca9c32a0f1732d38f1927fb3d069
parwer <55444278+parwer@users.noreply.github.com>
2023-05-12 19:44:28 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -2,35 +2,40 @@ import keras
 from keras_preprocessing.sequence import pad_sequences
 from keras_preprocessing.text import tokenizer_from_json
 import json
+from googletrans import Translator
+translator = Translator()
 
+#import tokenizer
 with open('results/tokenizer.json') as f:
     data = json.load(f)
     tokenizer = tokenizer_from_json(data)
 
+#import model
 model = keras.models.load_model('results/model.h5')
 
 def predict(txt):
-    word2vec = pad_sequences(tokenizer.texts_to_sequences([txt]), maxlen=30)
+    txt = translator.translate(txt, dest='en')
+    word2vec = pad_sequences(tokenizer.texts_to_sequences([txt.text]), maxlen=30)
     predict = model.predict(word2vec)
     return set_sentiment(predict)
 
 def set_sentiment(vector):
-    if vector > 0.5:
+    if vector > 0.7:
         return "positive"
-    else:
+    elif vector < 0.4:
         return "negative"
+    else:
+        return "neural"
 
 
-
+#webAPI
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
 origins = [
     "*"
 ]
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
